aadd4c5a3015bd4960c62dfb4fd62de8fc40d8fa
liuwei <liuwei@liuweideMacBook-Pro.local>
2023-12-07 15:59:27 +0800

    添加账号密码限制

--- Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UILogin/UILoginComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UILogin/UILoginComponentSystem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace ET.Client
@@ -21,6 +22,17 @@ namespace ET.Client
 		
 		public static void OnLogin(this UILoginComponent self)
 		{
+			var account = self.account.GetComponent<InputField>().text;
+			var password = self.password.GetComponent<InputField>().text;
+			//Account限制在4-8位数，不能有下划线及其他特殊字符
+			var account_IsMatch = Regex.IsMatch(account, @"\b(?!-_)\w[4,8]\b");
+			//Password限制：
+			//开头大写，限制在6-12位数，不包含特殊字符
+			var password_IsMatch = Regex.IsMatch(password, @"\b(?!-_)\w[6,12]\b");
+			
+			if (!account_IsMatch || !password_IsMatch)
+				return;
+			
 			LoginHelper.Login(
 				self.Root(), 
 				self.account.GetComponent<InputField>().text, 
